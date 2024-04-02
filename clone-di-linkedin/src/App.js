ae3208f8edeb9982d50823f84593b8adfcf1ac2c
GiuliaLanari <giulia.lanari96@gmail.com>
2024-04-02 15:42:47 +0200

    Update App.js
    struttura App

--- clone-di-linkedin/src/App.js
+++ clone-di-linkedin/src/App.js
@@ -1,9 +1,31 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Footer from "./components/Footer";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 function App() {
-  return <Footer />;
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col id="navbar"></Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row className="justify-content-between g-0">
+          <Col xs={9} id="topMain"></Col>
+          <Col xs={3} className=" p-2" id="side"></Col>
+        </Row>
+        <Row>
+          <Col xs={12} id="footer">
+            <Footer />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default App;
