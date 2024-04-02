eb9ca0481c03154a5a8d5da7f77412aa2d044693
SimoCrisce <126293808+SimoCrisce@users.noreply.github.com>
2024-04-02 11:57:14 +0200

    first commit

---
+++ clone-di-linkedin/src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+import ListGroup from "react-bootstrap/ListGroup";
+
+const SideBar = function () {
+  return (
+    <ListGroup>
+      <ListGroup.Item>aaaaaaaaaaaaaa</ListGroup.Item>
+      <ListGroup.Item></ListGroup.Item>
+    </ListGroup>
+  );
+};
+
+export default SideBar;
