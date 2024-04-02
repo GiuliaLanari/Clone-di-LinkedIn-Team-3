51e027941734ce4def54facd4062e4aa75a449aa
Innocenzo Merola <146184569+InnocenzoMerola@users.noreply.github.com>
2024-04-02 12:47:55 +0200

    installation app.js

--- clone-di-linkedin/src/App.js
+++ clone-di-linkedin/src/App.js
@@ -1,8 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col id="navbar"></Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row className="justify-content-between">
+          <div id="topMain"></div>
+          <div id="side"></div>
+        </Row>
+        <Row>
+          <Col xs={12} id="footer"></Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default App;
