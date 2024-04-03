a24bdba42b38c9c6a38878499dba49c1a48c2413
Mia <155841134+miaansaloni@users.noreply.github.com>
2024-04-03 19:07:59 +0200

    updates app.js

--- clone-di-linkedin/src/App.js
+++ clone-di-linkedin/src/App.js
@@ -3,35 +3,15 @@ import "./App.css";
 import Footer from "./components/Footer";
 import MyNavbar from "./components/MyNavbar";
 import Messagistica from "./components/Messagistica";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import ProfileInfoTop from "react-bootstrap/ProfileInfoTop";
-import ProfileInfoBottom from "react-bootstrap/ProfileInfoBottom";
-import SideBar from "./components/SideBar";
-
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
     <>
       <MyNavbar />
-      <Container>
-        <Row className="justify-content-between g-0">
-          <Col xs={9}>
-            <ProfileInfoTop />
-            <ProfileInfoBottom />
-          </Col>
-          <Col xs={3} className="p-2">
-            <SideBar />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <Footer />
-          </Col>
-        </Row>
-        <Messagistica/>
-      </Container>
+      <ProfilePage />
+      <Footer />
+      <Messagistica />
     </>
   );
 }
