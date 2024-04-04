1931f36ae51224187d11ae539c37cdf7c80967ca
Mia <155841134+miaansaloni@users.noreply.github.com>
2024-04-04 10:04:19 +0200

    setting up environment

--- clone-di-linkedin/src/App.js
+++ clone-di-linkedin/src/App.js
@@ -1,19 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Footer from "./components/Footer";
 import MyNavbar from "./components/MyNavbar";
 import Messagistica from "./components/Messagistica";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProfilePage from "./components/ProfilePage";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <MyNavbar />
-      <ProfilePage />
-      <Footer />
+      <Routes>
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
       <Messagistica />
-    </>
+    </BrowserRouter>
   );
 }
 
